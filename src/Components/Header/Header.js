e40a364fd78a2beec9d568afbab32e46d7f995ed
azhhng <azhhng@gmail.com>
2023-05-28 12:18:54 -0400

    remove .env file

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,20 @@
-import './Header.css';
-import { Link } from 'react-router-dom';
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  return (
+    <div className="header-container">
+      <h1> Hi, I'm Alice!</h1>
+      <h2>
+        <Link to="/">Home</Link> \\{" "}
+        <a href="https://drive.google.com/file/d/1T_tXuDMo129WKmUNMH19XFe9HhyGPYkF/view?usp=sharing">
+          Resume
+        </a>{" "}
+        \\ <a href="http://github.com/azhhng">Github</a> \\{" "}
+        <a href="mailto:mingg.zhang@mail.utoronto.ca">Email</a>
+      </h2>
+    </div>
+  );
+};
 
-    return (
-        <div className="header-container">
-            <h1> Hi, I'm Alice Zhang!</h1>
-            <h2><Link to="/">Home</Link> \\ <a href="https://drive.google.com/file/d/1T_tXuDMo129WKmUNMH19XFe9HhyGPYkF/view?usp=sharing">Resume</a> \\ <a href="http://github.com/azhhng">Github</a> \\ <a href="mailto:mingg.zhang@mail.utoronto.ca">Email</a></h2>
-        </div>
-    )
-}
-
-export default Header
+export default Header;
