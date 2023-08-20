938a1d54a6ef382c3c146eac191b2af749d53650
azhhng <azhhng@gmail.com>
2023-08-19 21:46:59 -0400

    condense header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ import Emoji from "../Emoji/Emoji";
 const Header = () => {
   return (
     <div className="header-container">
-      <div className="header-title">
+      <div className="header-picture">
         <img
           src={require(`../../Assets/Images/profile.jpg`).default}
           alt="ponyo"
@@ -13,49 +13,51 @@ const Header = () => {
           height={"120px"}
           id="profile-pic"
         />
-        <h1>
-          Hi, I'm Alice!
-          <Emoji
-            name={"flower"}
-            width={25}
-            height={25}
-            style={{
-              position: "relative",
-              bottom: "-45px",
-              right: "360px",
-              backgroundColor: "#ffd7d4",
-              border: "2px solid #ff8f87",
-              padding: "7px",
-              borderRadius: "50px",
-            }}
-          />
-        </h1>
+        <Emoji
+          name={"flower"}
+          width={25}
+          height={25}
+          style={{
+            position: "relative",
+            bottom: "-0px",
+            right: "40px",
+            backgroundColor: "#ffd7d4",
+            border: "2px solid #ff8f87",
+            padding: "7px",
+            borderRadius: "50px",
+          }}
+        />
       </div>
 
-      <div className="header-links">
-        <Link to="/">Home</Link>
-        \\
-        <a
-          href="https://drive.google.com/file/d/1T_tXuDMo129WKmUNMH19XFe9HhyGPYkF/view?usp=sharing"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Resume
-        </a>
-        \\
-        <a href="http://github.com/azhhng" target="_blank" rel="noreferrer">
-          Github
-        </a>
-        \\
-        <a
-          href="https://www.linkedin.com/in/azhhng/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          LinkedIn
-        </a>
-        \\
-        <a href="mailto:mingg.zhang@mail.utoronto.ca">Email</a>
+      <div className="header-text">
+        <div className="header-title">
+          <h1>Hi, I'm Alice!</h1>
+        </div>
+        <div className="header-links">
+          <Link to="/">Home</Link>
+          \\
+          <a
+            href="https://drive.google.com/file/d/1T_tXuDMo129WKmUNMH19XFe9HhyGPYkF/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resume
+          </a>
+          \\
+          <a href="http://github.com/azhhng" target="_blank" rel="noreferrer">
+            Github
+          </a>
+          \\
+          <a
+            href="https://www.linkedin.com/in/azhhng/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+          \\
+          <a href="mailto:mingg.zhang@mail.utoronto.ca">Email</a>
+        </div>
       </div>
     </div>
   );
