b3d0215dbe31c46d2fe8dca5bb7d1673693e7865
alizhang <alizhang@hubspot.com>
2025-07-07 10:45:53 -0600

    update project

--- src/Components/Emoji/Emoji.jsx
+++ src/Components/Emoji/Emoji.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Emoji(props) {
   return (
     <img
-      src={require(`../../Assets/Images/${props.name}.svg`).default}
+      src={require(`../../Assets/Images/${props.name}.svg`)}
       alt="ponyo"
       width={props.width}
       height={props.height}
